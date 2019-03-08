18f655a871c4cc69350f89ebfba01d63c4c4266c
Jason <23126557+dyckia@users.noreply.github.com>
2019-03-08 17:19:24 -0330

    solved P203

---
+++ 203_remove_linked_list_elements/leetcode_203.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        // if the next val is the value we want to revome, skip that node
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode cur = dummy;
+
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+}
