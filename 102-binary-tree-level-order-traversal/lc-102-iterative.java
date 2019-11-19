d5bc108d8f4e468f5e8026de6244e768572bd4bf
Jason <23126557+dyckia@users.noreply.github.com>
2019-11-19 20:16:30 -0330

    Solve p102

---
+++ 102-binary-tree-level-order-traversal/lc-102-iterative.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> curRow = new ArrayList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode curNode = queue.poll();
+                curRow.add(curNode.val);
+                if (curNode.left != null) queue.offer(curNode.left);
+                if (curNode.right != null) queue.offer(curNode.right);
+            }
+
+            res.add(curRow);
+        }
+
+        return res;
+    }
+}
---
+++ 102-binary-tree-level-order-traversal/lc-102-recursive.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        
+        preorder(root, 1, res);
+
+        return res;
+    }
+
+    // build res recursively using preorder traversal
+    private void preorder(TreeNode root, int level, List<List<Integer>> res) {
+        if (root == null) return;
+
+        // cur row does not exist, create a new row
+        if (res.size() < level) {
+            List<Integer> newRow = new ArrayList<>();
+            res.add(newRow);
+        }
+
+        // retrieve the row of this level
+        List<Integer> curRow = res.get(level - 1);
+
+        // add root value to current row
+        curRow.add(root.val);
+
+        // recursively add left and right node if not null
+        preorder(root.left, level + 1, res);
+        preorder(root.right, level + 1, res);
+
+        return;
+    }
+}
