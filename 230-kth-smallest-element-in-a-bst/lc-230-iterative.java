25f9e8d27c76e2c95724d4044680c06d0caf8f51
Jason <23126557+dyckia@users.noreply.github.com>
2019-11-23 15:48:47 -0330

    Solve p230

---
+++ 230-kth-smallest-element-in-a-bst/lc-230-iterative.java
@@ -0,0 +1,23 @@
+// inorder traversal outputs the values in BST in ascending order
+
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+
+        while (!stack.isEmpty() || cur != null) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+
+            cur = stack.pop();
+            k--;
+            if (k == 0) return cur.val;
+
+            cur = cur.right;
+        }
+        
+        return -1;
+    }
+}
---
+++ 230-kth-smallest-element-in-a-bst/lc-230-recursive.java
@@ -0,0 +1,24 @@
+// inorder traversal outputs the values in BST in ascending order
+
+class Solution {
+
+    int ans;
+    int count;
+
+    public int kthSmallest(TreeNode root, int k) {
+        ans = -1;
+        count = k;
+        inorder(root);
+        return ans;
+    }
+
+    public void inorder(TreeNode root) {
+        if (root == null) return;
+        
+        inorder(root.left);
+        count--;
+        if (count == 0) ans = root.val;
+        inorder(root.right);
+        return;
+    }
+}
