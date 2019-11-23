ed1a5052954af1b690f38011c101b9bcd74efa52
Jason <23126557+dyckia@users.noreply.github.com>
2019-11-22 22:20:48 -0330

    Solve p285

---
+++ 285-inorder-successor-in-bst/lc-285-recursive.java
@@ -0,0 +1,11 @@
+class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if (root == null) return null;
+
+        if (root.val <= p.val) return inorderSuccessor(root.right, p);
+
+        // root.val > p.val
+        TreeNode smallerNode = inorderSuccessor(root.left, p);
+        return (smallerNode == null) ? root : smallerNode;
+    }
+}
---
+++ 285-inorder-successor-in-bst/lc-285.java
@@ -0,0 +1,18 @@
+class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+
+        TreeNode candidate = null;
+        TreeNode cur = root;
+
+        while (cur != null) {
+            if (cur.val > p.val) {
+                candidate = cur;
+                cur = cur.left;
+            } else if (cur.val <= p.val) {
+                cur = cur.right;
+            }
+        }
+
+        return candidate;
+    }
+}
