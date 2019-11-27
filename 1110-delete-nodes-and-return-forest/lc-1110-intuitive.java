f613562dbde982ac2e4ae7db5a60ffdac29a69f8
Jason <23126557+dyckia@users.noreply.github.com>
2019-11-26 21:11:26 -0330

    Solve P1110

---
+++ 1110-delete-nodes-and-return-forest/lc-1110-intuitive.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        Set<Integer> toDelSet = new HashSet<>();
+        addToSet(to_delete, toDelSet);
+
+        List<TreeNode> res = new LinkedList<>();
+        postOrder(root, toDelSet, res);
+
+        // check if root needs to be deleted
+        if (root != null && toDelSet.contains(root.val)) delNode(root, res);
+        else res.add(root);
+
+        return res;
+    }
+
+    private void addToSet(int[] to_delete, Set<Integer> toDelSet) {
+        for (int val : to_delete) {
+            toDelSet.add(val);
+        }
+        return;
+    }
+
+    // post order to find the parent of node to be deleted
+    private void postOrder(TreeNode node, Set<Integer> toDelSet, List<TreeNode> res) {
+        if (node == null) return;
+
+        postOrder(node.left, toDelSet, res);
+        postOrder(node.right, toDelSet, res);
+
+        if (node.left != null && toDelSet.contains(node.left.val)) {
+            node.left = delNode(node.left, res);
+        }
+
+        if (node.right != null && toDelSet.contains(node.right.val)) {
+            node.right = delNode(node.right, res);
+        }
+
+        return;
+    }
+
+    // return the new node after deletion
+    private TreeNode delNode(TreeNode nodeToDel, List<TreeNode> res) {
+        // if nodeToDel's children are not null
+        // add them in res
+
+        if (nodeToDel.left != null) res.add(nodeToDel.left);
+        if (nodeToDel.right != null) res.add(nodeToDel.right);
+
+        return null;
+    }
+}
---
+++ 1110-delete-nodes-and-return-forest/lc-1110-recursive.java
@@ -0,0 +1,31 @@
+class Solution {
+    Set<Integer> toDelSet;
+    List<TreeNode> res;
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        toDelSet = new HashSet<>();
+        for (int val : to_delete) {
+            toDelSet.add(val);
+        }
+
+        res = new LinkedList<>();
+        helper(root, true);
+
+        return res;
+    }
+
+    // return the node itself if not be deleted, null otherwise
+    private TreeNode helper(TreeNode node, boolean isRoot) {
+        if (node == null) return null;
+
+        boolean toDelete = toDelSet.contains(node.val);
+
+        if (isRoot && !toDelete) res.add(node);
+        
+        // if node is to be deleted, its children are root
+        node.left = helper(node.left, toDelete);
+        node.right = helper(node.right, toDelete);
+
+        return toDelete ? null : node;
+    }
+}
