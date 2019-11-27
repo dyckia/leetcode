62d69db79aa460271d8dd25b595c52c618184eb6
Jason <23126557+dyckia@users.noreply.github.com>
2019-11-27 18:43:42 -0330

    Solve P298

---
+++ 298-binary-tree-longest-consecutive-sequence/lc-298-bottom-up.java
@@ -0,0 +1,29 @@
+class Solution {
+    int maxL;
+    
+    public int longestConsecutive(TreeNode root) {
+        maxL = 0;
+        
+        postOrder(root);
+        
+        return maxL;
+    }
+    
+    // return the longest consecutive length starting from current node
+    private int postOrder(TreeNode cur) {
+        if (cur == null) return 0;
+        
+        // cur is not null
+        int len = 1;
+        
+        int left = postOrder(cur.left);
+        if (left != 0 && cur.val + 1 == cur.left.val) len = Math.max(len, left + 1);
+        
+        int right = postOrder(cur.right);
+        if (right != 0 && cur.val + 1 == cur.right.val) len = Math.max(len, right + 1);
+        
+        maxL = Math.max(maxL, len);
+        
+        return len;
+    }
+}
---
+++ 298-binary-tree-longest-consecutive-sequence/lc-298-top-down.java
@@ -0,0 +1,25 @@
+class Solution {
+    int maxL;
+    
+    public int longestConsecutive(TreeNode root) {
+        maxL = 0;
+        
+        preOrder(root, null, 1);
+        
+        return maxL;
+    }
+    
+    private void preOrder(TreeNode cur, TreeNode par, int len) {
+        if (cur == null) return;
+        
+        if (par != null && cur.val == par.val + 1) len++;
+        else len = 1;
+        
+        maxL = Math.max(maxL, len);
+
+        preOrder(cur.left, cur, len);
+        preOrder(cur.right, cur, len);
+        
+        return;
+    }
+}
