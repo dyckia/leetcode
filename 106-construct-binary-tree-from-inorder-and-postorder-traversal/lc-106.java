55dea327df230d2535fe8816c1b795ea2a088edd
Jason <23126557+dyckia@users.noreply.github.com>
2019-11-28 11:50:16 -0330

    Solve P106

---
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/lc-106.java
@@ -0,0 +1,35 @@
+class Solution {
+    int[] postArr;
+    int postInd;
+    Map<Integer, Integer> inIndMap;
+    
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        postArr = postorder;
+        postInd = postArr.length - 1;
+        inIndMap = new HashMap<>();
+        addToMap(inorder);
+        return helper(0, inorder.length - 1);
+    }
+
+
+    private TreeNode helper(int inStart, int inEnd) {
+        if (postInd < 0 || inStart > inEnd) return null;
+
+        TreeNode node = new TreeNode(postArr[postInd]);
+        postInd--;
+        int inInd = inIndMap.get(node.val);
+
+        // start from right subtree
+        node.right = helper(inInd + 1, inEnd);
+        node.left = helper(inStart, inInd - 1);
+
+        return node;
+    }
+
+    private void addToMap(int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            inIndMap.put(inorder[i], i);
+        }
+        return;
+    }
+}
