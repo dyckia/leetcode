e6086daae76e5912dc34f4f357a7a11ccecbbb0d
Jason <23126557+dyckia@users.noreply.github.com>
2020-04-26 23:30:38 -0230

    Update solutions

--- 48-rotate-image/lc-48-simulation.java
+++ 48-rotate-image/lc-48-simulation.java
@@ -1,36 +1,38 @@
 class Solution {
 
     /*
-     * we rotate the matrix layer by layer
+     * we rotate the matrix layer by layer (from outer layer to innner layer)
      * 
-     * 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+     *  1  2  3  4  5 
+     *  6  7  8  9 10 
+     * 11 12 13 14 15 
+     * 16 17 18 19 20 
+     * 21 22 23 24 25
      * 
-     * 1 2 3 4 5 6 10 11 15 16 20 21 22 23 24 25
      * 
-     * we start from (c, c) the cordinates of other three elements are (c, n - 1 -
-     * c) (n - 1 - c, n - 1 - c) (n - 1 - c, c)
+     * For element [r][c] the swaps are:
+     * [r][c] -> [c][n - 1 - r] -> [n - 1 - r, n - 1 - c] -> [n - 1 - c, r]
      * 
-     * next element is (c, c + 1) the cordinates of other three elements are (c + 1,
-     * n - 1 - c) (n - 1 - c, n - 1 - c - 1) (n - 1 - c - 1, c)
+     * for row, end condition is row < n / 2
      * 
-     * the end condition for this layer is when c + k == n - 1 - c
+     * for column, end condition is col < n - row - 1
      */
 
     public void rotate(int[][] matrix) {
 
         int n = matrix.length;
 
-        for (int layer = 0; layer <= n / 2 - 1; layer++) {
-            int end = n - 1 - layer;
-            for (int i = 0; i < end - layer; i++) {
-                int temp = matrix[layer][layer + i];
-                matrix[layer][layer + i] = matrix[end - i][layer];
-                matrix[end - i][layer] = matrix[end][end - i];
-                matrix[end][end - i] = matrix[layer + i][end];
-                matrix[layer + i][end] = temp;
+        for (int row = 0; row < n / 2; row++) {
+            for (int col = row; col < n - row - 1; col++) {
+                int temp = matrix[row][col];
+                matrix[row][col] = matrix[n - 1 - col][row];
+                matrix[n - 1 - col][row] = matrix[n - 1 - row][n - 1 - col];
+                matrix[n - 1 - row][n - 1 - col] = matrix[col][n - 1 - row];
+                matrix[col][n - 1 - row] = temp;
             }
         }
 
+        return;
     }
 
 }
