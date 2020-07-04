33e064b18c5e5dd17a62e5bbc67030ed8535d9e2
Jason <23126557+dyckia@users.noreply.github.com>
2020-07-04 15:38:32 -0230

    Solve p7

---
+++ 7-reverse-integer/lc-7.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+        int res = 0;
+        
+        while (x != 0) {
+            int rem = x % 10;
+            x = x / 10;
+            if (res > Integer.MAX_VALUE / 10 || res < Integer.MIN_VALUE / 10) return 0;
+            if (res == Integer.MAX_VALUE && rem > Integer.MAX_VALUE % 10) return 0;
+            if (res == Integer.MIN_VALUE && rem < Integer.MIN_VALUE % 10) return 0;
+            res = res * 10 + rem;
+        }
+        
+        return res;
+    }
+}
