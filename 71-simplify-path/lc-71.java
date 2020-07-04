41b62616106cfdd59a97101fc09a2c13993acf42
Jason <23126557+dyckia@users.noreply.github.com>
2020-07-04 13:02:37 -0230

    Solve p71

--- 71-simplify-path/lc-71.java
+++ 71-simplify-path/lc-71.java
@@ -1,5 +1,29 @@
 class Solution {
     public String simplifyPath(String path) {
-
+        Deque<String> deque = new ArrayDeque<String>();
+        String[] components = path.split("/");
+        
+        for (String component : components) {
+            // empty string or "."
+            if (component.isEmpty() || component.equals(".")) continue;
+            if (component.equals("..")) {
+                // .. pop last element if not empty
+                // deque.removeFirst();
+                if (!deque.isEmpty()) deque.pop();
+            } else {
+                // deque.addFirst(component);
+                deque.push(component);
+            }
+            
+        }
+        
+        // re-join each component
+        StringBuilder sb = new StringBuilder();
+        while (!deque.isEmpty()) {
+            sb.append("/");
+            sb.append(deque.removeLast());
+        }
+        
+        return sb.length() == 0 ? "/" : sb.toString();
     }
 }
