e2e171bbf69f53537bb81eea9896d1d7987c6934
Jason <23126557+dyckia@users.noreply.github.com>
2020-07-08 23:04:21 -0230

    Solve p349

---
+++ 349-intersection-of-two-arrays/lc-349-retainall.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = addToSet(nums1);
+        Set<Integer> set2 = addToSet(nums2);
+        
+        set1.retainAll(set2);
+        int[] res = new int[set1.size()];
+        int i = 0;
+        for (int n : set1) {
+            res[i++] = n;
+        }
+        return res;
+    }
+    
+}
---
+++ 349-intersection-of-two-arrays/lc-349.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = addToSet(nums1);
+        Set<Integer> set2 = addToSet(nums2);
+        
+        if (set1.size() < set2.size())
+            return setInter(set1, set2);
+        else 
+            return setInter(set2, set1);
+    }
+    
+    private Set<Integer> addToSet(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            set.add(n);
+        }
+        return set;
+    }
+    
+    private int[] setInter(Set<Integer> set1, Set<Integer> set2) {
+        // set1.size() < set2.size()
+        // or initialize as array and use Arrays.copyOf() method
+        List<Integer> list = new ArrayList<>();
+        
+        for (int num : set1) {
+            if (set2.contains(num)) list.add(num);
+        }
+        
+        // can not use toArray() method as Integer can not be converted to int
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        
+        return res;
+    }
+}
